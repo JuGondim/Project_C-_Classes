62b6521fa2e401725fcd5c0d7bc6ff68445fb6ed
Juliana <juh.ngn@gmail.com>
2022-08-30 06:17:33 -0300

    Adicionando projeto de Classes

---
+++ Pokedex/Program.cs
@@ -0,0 +1,25 @@
+﻿using Pokedex.Src;
+using System;
+
+namespace Pokedex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vinda(o) a sua Pokedex! \n");            
+
+            Pokemon squirtle = new Pokemon(007, "Squirtle", "Água", 02, "Elétrico/Grama");
+            Pokemon jigglypuff = new Pokemon(039, "Jigglypuff", "Normal/Fada", 02, "Aço/Veneno");
+            Pokemon psyduck = new Pokemon(054, "Psyduck", "Água", 01, "Elétrico/Grama");
+            Pokemon mankey = new Pokemon(056, "Mankey", "Lutador", 01, "Fada/Psíquico/Voador");
+            Pokemon slowpoke = new Pokemon(079, "Slowpoke", "Água/Psíquico", 02, "Elétrico/Fantasma/Grama/Inseto/Sombrio");
+
+            Console.WriteLine(squirtle.ToString());
+            Console.WriteLine(jigglypuff.ToString());
+            Console.WriteLine(psyduck.ToString());
+            Console.WriteLine(mankey.ToString());
+            Console.WriteLine(slowpoke.ToString());
+        }
+    }
+}
---
+++ Pokedex/Src/Pokemon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Src
+{
+    internal class Pokemon
+    {
+        int Id_Pokedex { get; set; }
+        string Nome { get; set; }
+        string Tipo { get; set; }
+        int Evoluções { get; set; }
+        string Fraquezas { get; set; }
+
+        public Pokemon(int id_Pokedex, string nome, string tipo, int evoluções, string fraquezas)
+        {
+            Id_Pokedex = id_Pokedex;
+            Nome = nome;
+            Tipo = tipo;
+            Evoluções = evoluções;
+            Fraquezas = fraquezas;
+        }
+
+        public override string ToString()
+        {
+            return $"Lista de Pokemons capturados: Id na Pokedex {Id_Pokedex} \n" +
+                   $"Nome: {Nome} \n" +
+                   $"Tipo: {Tipo} \n" +
+                   $"Evoluções: {Evoluções} \n" +
+                   $"Fraquezas: {Fraquezas} \n";
+        }
+    }
+}
